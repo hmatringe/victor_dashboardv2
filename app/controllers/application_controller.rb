4dc38230b3b637657ffcc76448b4ad19b4283663
Matringe <hadrien.matringe@gmail.com>
2018-01-05 21:53:35 +0100

    redirection fix after sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     # current_user_path # default
-    streams_path
+    projects_path
   end
 
   def set_path
